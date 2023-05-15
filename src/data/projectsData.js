8fb06ce9ff7a3877ed8cab6ae527c55f5728be5b
Janvi Bajoria <janvibajo1@gmail.com>
2023-05-15 17:38:22 +0530

    added projects data

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,36 +1,39 @@
 export const projectsData = [
   {
     projectname: "Maid Aid",
-    description: "A one-stop solution to hire helpers for your day-to-day service.",
-    techstack: ["React", "Firebase"],
+    description:
+      "A one-stop solution to hire helpers for your day-to-day service.",
+    techstack: ["React", "Firebase", "CSS"],
     githuburl: "",
     liveurl: "",
   },
   {
     projectname: "Co - Alert",
-    description: "lorem ipsum donor sital altor lorem ipsum",
-    techstack: ["first", "second"],
+    description:
+      "A website which search for corona vaccine availaibility in your district for a specified date.",
+    techstack: ["React", "CoWin API", "Chakra UI"],
     githuburl: "",
     liveurl: "",
   },
   {
     projectname: "QuoteSite",
-    description: "lorem ipsum donor sital altor lorem ipsum",
-    techstack: ["first", "second"],
+    description: "A collection of quotes of different categories.",
+    techstack: ["React", "Firebase", "Chakra UI"],
     githuburl: "",
     liveurl: "",
   },
   {
     projectname: "TakeBreak",
-    description: "lorem ipsum donor sital altor lorem ipsum",
-    techstack: ["first", "second"],
+    description:
+      "A Chrome extension that reminds you to take break at a specified interval.",
+    techstack: ["HTML", "CSS", "JavaScript", "ChromeTools"],
     githuburl: "",
     liveurl: "",
   },
   {
     projectname: "QuizKy",
-    description: "lorem ipsum donor sital altor lorem ipsum",
-    techstack: ["first", "second"],
+    description: "A website to play random quiz of different categories.",
+    techstack: ["React", "Chakra UI"],
     githuburl: "",
     liveurl: "",
   },
