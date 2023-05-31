d6cf9a0c0d444a2b1a4d6ffa92ab707192db50f8
Janvi Bajoria <janvibajo1@gmail.com>
2023-05-31 16:53:12 +0530

    added google font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,13 +3,14 @@ import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 import { ChakraProvider, Box, VStack } from "@chakra-ui/react";
 import theme from "../../theme";
+import styles from "../styles/Home.module.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <div className="app">
+      <div className={styles.app}>
         <VStack>
-          <Box p={["4", "8", "16"]} minH={"100vh"} w={["100%", "90%", "75%"]}>
+          <Box p={["4", "8", "16"]} minH={"100vh"} w={["100%", "90%", "70%"]}>
             <Navbar />
             <Component {...pageProps} />
             <Footer />
